dbc697f3783dbc4ea299ad7eec62aad7257052ad
Jeremias-V <jeremias.villalobos.t@gmail.com>
2024-05-13 16:14:02 -0500

    Fix tests

--- lamba_function.py
+++ lamba_function.py
@@ -3,6 +3,6 @@ import json
 def lambda_handler(event, context):
     # TODO implement the logic
     return {
-        'statusCode': 400,
+        'statusCode': 200,
         'body': json.dumps('Method not not implemented yet!')
     }
