c6fc7cb260104e4b91195509c73ab7bc9fb5ced8
Jeremias-V <jeremias.villalobos.t@gmail.com>
2024-05-13 16:38:47 -0500

    Test new response

--- lamba_function.py
+++ lamba_function.py
@@ -1,8 +1,10 @@
 import json
+import numpy as np
 
 def lambda_handler(event, context):
     # TODO implement the logic
+    random_int = np.random.randint(10**6, size=2)[0]
     return {
         'statusCode': 200,
-        'body': json.dumps('Method not not implemented yet!')
+        'body': json.dumps('The random number generated was ' + str(random_int))
     }
